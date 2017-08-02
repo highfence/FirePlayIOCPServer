ade2b0cdea7ca06c8e3fb8240630b0f1af4bef59
Lee Kwenwon <highfence@naver.com>
2017-08-02 14:47:07 +0900

    [ERROR] Login 로직 프로세스 진행중 에러 발견. 37라인 이후로 진행 불가.

--- LogicLib/PacketProcessLogin.cpp
+++ LogicLib/PacketProcessLogin.cpp
@@ -13,26 +13,31 @@
 #include "UserManager.h"
 
 using PACKET_ID = FirePlayCommon::PACKET_ID;
+using LogType = FirePlayCommon::LogType;
 
 namespace FirePlayLogic
 {
 	ERROR_CODE PacketProcess::login(std::shared_ptr<RecvPacketInfo> packetInfo)
 	{
-		_logger->Write(FirePlayCommon::LogType::LOG_DEBUG, "%s | Login Func Entry", __FUNCTION__);
+		_logger->Write(LogType::LOG_DEBUG, "%s | Login Func Entry", __FUNCTION__);
 
 		FirePlayCommon::PktLogInRes resPkt;
 		auto reqPkt = (FirePlayCommon::PktLogInReq*)packetInfo->pData;
 
 		auto addRet = _userManager->AddUser(packetInfo->SessionIndex, reqPkt->szID);
+		_logger->Write(LogType::LOG_DEBUG, "%s | Add User Session(%d), Id(%s)", __FUNCTION__, packetInfo->SessionIndex, reqPkt->szID);
 
 		if (addRet != ERROR_CODE::NONE)
 		{
 			// TODO :: Check error
+			_logger->Write(LogType::LOG_ERROR, "%s | AddUser Failed, ERROR_CODE(%d)", __FUNCTION__, static_cast<int>(addRet));
 		}
 
+		// ERROR :: 이 이상 진행 불가. 데드락?
 		_connectedUserManager->SetLogin(packetInfo->SessionIndex);
 
 		resPkt.ErrorCode = (short)addRet;
+		_logger->Write(LogType::LOG_DEBUG, "%s | AddUser ERROR_CODE(%d)", __FUNCTION__, static_cast<int>(addRet));
 
 		RecvPacketInfo sendPacket;
 		sendPacket.SessionIndex = packetInfo->SessionIndex;
@@ -40,6 +45,7 @@ namespace FirePlayLogic
 		sendPacket.PacketBodySize = sizeof(FirePlayCommon::PktLogInRes);
 		sendPacket.pData = (char*)&resPkt;
 
+		_logger->Write(LogType::LOG_DEBUG, "%s | Push Packet(%d) to SendQueue", __FUNCTION__, sendPacket.PacketId);
 		_sendQueue->Push(std::make_shared<RecvPacketInfo>(sendPacket));
 
 		return ERROR_CODE::NONE;
